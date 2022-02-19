60c87c18d0b5ff28600abd1f01c844abbe618932
Kris <Kristopher.Kath@gmail.com>
2022-02-18 19:31:45 -0800

    Created a unit spawner. Hooked unit spawner to be created for each client. Learned about NetworkServer.Spawn() to spawn prefabs with an owner on each client

---
+++ Assets/Scripts/Buildings/UnitSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.EventSystems;
+
+public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject unitPrefab;
+    [SerializeField] private Transform spawnPoint;
+
+
+
+
+
+
+    #region Server
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        GameObject unitInstance = Instantiate(unitPrefab, spawnPoint.position, spawnPoint.rotation); //Server will run this, but not clients
+
+        //The given gameobject will be spawned on each each client. Must be a registered prefab on NetworkManager. 
+            //connectionToClient is the client that called this. So that client will be the owner.
+        NetworkServer.Spawn(unitInstance, connectionToClient); 
+    }
+
+    #endregion
+
+    #region Client
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Left) return;
+        if (!hasAuthority) return;
+
+        CmdSpawnUnit();
+    }
+
+    #endregion
+}
--- Assets/Scripts/Networking/RTSNetworkingManager.cs
+++ Assets/Scripts/Networking/RTSNetworkingManager.cs
@@ -5,5 +5,20 @@ using Mirror;
 
 public class RTSNetworkingManager : NetworkManager
 {
-    
+    [SerializeField] GameObject unitSpawnerPrefab;
+
+    //Occurs after the player is created
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        //Spawn the unit spawner prefab
+        GameObject unitSpawnerInstance = Instantiate(
+            unitSpawnerPrefab, 
+            conn.identity.transform.position, 
+            conn.identity.transform.rotation);
+
+        //Spawn gameobject for each client and give owner to prefab
+        NetworkServer.Spawn(unitSpawnerInstance, conn);
+    }
 }
