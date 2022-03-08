fae32dd9247a27f3c0724dead5f14a0419044afd
Kristopher Kath <Kristopher.Kath@gmail.com>
2022-03-07 20:28:31 -0800

    Setup collision layers and look at camera script for health bars

---
+++ Assets/Scripts/Cameras/FaceCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private Transform mainCameraTransform;
+
+    void Start()
+    {
+        mainCameraTransform = Camera.main.transform;    
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(
+            transform.position + mainCameraTransform.rotation * Vector3.forward, //cameras forward vector
+            mainCameraTransform.rotation * Vector3.up); //cameras up vector
+    }
+}
